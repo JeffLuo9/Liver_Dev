26b843cc91e2314d7153989d7c0946eebee01f2d
Jeff Luo <1042805778@qq.com>
2026-09-01 11:16:03 +0800

    Update and rename 01.hep_ploidy_reg.r to 02.hep_ploidy_reg.r

--- 02.hep_ploidy_reg.r
+++ 02.hep_ploidy_reg.r
@@ -26,7 +26,7 @@ df_aggregated <- df %>%
     rownames(df_filter)<-df_filter[,1]
 
     return(df_filter)
-}
+}    
 
 
 dtb_file<-Sys.glob("liver_image/*dtb.csv")
@@ -34,6 +34,9 @@ for (i in c(1:length(dtb_file))){
     df<-read.csv(dtb_file[i])
     assign(paste0("df_",i),df)
 }
+
+sample_name1<-basename(dtb_file)
+sample_name1<-gsub("_dtb\\.csv","",sample_name1)
 
 for (i in c(1:length(dtb_file))){
     df<-get(paste0("df_",i))
@@ -43,8 +46,8 @@ for (i in c(1:length(dtb_file))){
     dfs$ploidy[dfs$ploidy == 6] = 4
 
     dfs$ploidy_counts<-paste0(dfs$ploidy,"_",dfs$dapi_counts)
-    assign(paste0("dfsn",i),dfs)
-}
+    assign(paste0("dfs_",sample_name1[i]),dfs)
+}   # Obtain ploidy information for all chips.
 
 gem2rds<-function(gem_path){
 gem=fread(gem_path)
@@ -60,4 +63,16 @@ colnames(mat1)=names(cell)
     return(obj1)
 } 
 
+gem_path<-Sys.glob("../*.gem.gz")
+sample_name<-basename(gem_path)
+sample_name<-gsub("\\.gem\\.gz","",sample_name)
+for (i in c(1:length(sample_name))){
+    rna<-gem2rds(gem_path[i])
+    assign(sample_name[i],rna)
+}
 
+Y00723K8<-AddMetaData(Y00723K8,dfs_Y00723K8))
+Y00723J6<-AddMetaData(Y00723J6,dfs_Y00723J6))
+Y00723E5<-AddMetaData(Y00723E5,dfs_Y00723E5))
+#......  and so on
+#The previously‑computed ploidy information was added to the Stereo‑cell single‑cell dataset to generate an analyzable file.
